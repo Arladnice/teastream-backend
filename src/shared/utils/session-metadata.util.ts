c0853f229b04c62ec38ad2ae6db826e744040c11
Denis Arlakov <darlakov@mail.ru>
2025-06-18 14:07:43 +0300

    feat: расширение функционала управления сессиями. Добавлены новые типы и методы для работы с сессиями, включая получение текущей сессии и удаление сессий пользователя. Обновлены модели данных для поддержки информации о местоположении и устройстве.

--- src/shared/utils/session-metadata.util.ts
+++ src/shared/utils/session-metadata.util.ts
@@ -1,11 +1,14 @@
 import type { Request } from 'express'
 import { lookup } from 'geoip-lite'
+import * as countries from 'i18n-iso-countries'
 
 import type { SessionMetadata } from '../types/session-metadata.types'
 
 import { IS_DEV_ENV } from './is-dev.util'
 
 import DeviceDetector = require('device-detector-js')
+
+countries.registerLocale(require('i18n-iso-countries/langs/ru.json'))
 
 export function getSessionMetadata(
 	req: Request,
@@ -25,7 +28,7 @@ export function getSessionMetadata(
 
 	return {
 		location: {
-			country: location.country || 'Неизвестно',
+			country: countries.getName(location.country, 'en') || 'Неизвестно',
 			city: location.city || 'Неизвестно',
 			latitude: location.ll[0] || 0,
 			longitude: location.ll[1] || 0
